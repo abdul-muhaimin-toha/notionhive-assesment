507db91540e916bf1d4ae2307b368cf2321a2b28
Abdul Muhaimin Toha <abdulmuhaimintoha.business@gmail.com>
2024-06-28 22:15:05 +0600

    slider design and function  done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+// /////////////////////////////////
+// Selecting Elements
+// /////////////////////////////////
+
 const mobileNavOpen = document.getElementById('mobile-nav-opener');
 const mobileNavClose = document.getElementById('mobile-nav-close');
 const mobileNav = document.getElementById('mobile-navigation');
+
+// /////////////////////////////////
+// Navigation Functionality
+// /////////////////////////////////
 
 mobileNavOpen.addEventListener('click', function () {
   mobileNav.style.display = 'flex';
@@ -8,4 +16,16 @@ mobileNavOpen.addEventListener('click', function () {
 
 mobileNavClose.addEventListener('click', function () {
   mobileNav.style.display = 'none';
+});
+
+// /////////////////////////////////
+// Slider Functionality
+// /////////////////////////////////
+
+$(document).ready(function () {
+  $('.slider').slick({
+    dots: true,
+    centerMode: true,
+    variableWidth: true,
+  });
 });
